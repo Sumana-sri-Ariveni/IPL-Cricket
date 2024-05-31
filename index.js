d2de3dbc5cfa4e387de6e07857f6eb4b95832415
Ariveni Sumana sri <110623376+Sumana-sri-Ariveni@users.noreply.github.com>
2024-05-31 21:20:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+    <div className="container">
+        <h1 className="heading">Super Over League</h1>
+        <div>
+            <img className="img1" src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" alt="Royal challengers" />
+            <img className="img1" src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="Super kings"/>
+        </div>
+        
+    </div>
+)
+ReactDOM.render(element,document.getElementById("root"))
